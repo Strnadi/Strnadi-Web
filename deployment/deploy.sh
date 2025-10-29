03a2f235fd80dc2d7563c916101bfb26b34d8442
Mdev <zdenda.hrouda+githubalt@outlook.cz>
2025-10-29 15:15:33 +0100

    oopsies 💅💅

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -10,33 +10,7 @@ git pull --rebase
 git submodule update --remote --init --merge --recursive
 
 # Port is the first and only argument to this script
-PORT=$1
-ENVIRONMENT=$2
-
-if [ -z "$PORT" ]; then
-  echo "Error: PORT argument is required."
-  exit 1
-fi
-
-if [ -z "$ENVIRONMENT" ]; then
-  echo "Error: ENVIRONMENT argument is required."
-  exit 1
-fi
-
-# Get the docker container ID that is bound to the specified port
-CONTAINER_ID=$(docker ps --filter "publish=$PORT" -q)
-
-# Check if a container was found
-if [ -n "$CONTAINER_ID" ]; then
-  docker kill "$CONTAINER_ID"
-
-  if [ $? -eq 0 ]; then
-    echo "Container successfully killed."
-  else
-    echo "Failed to kill container."
-    exit 1
-  fi
-fi
+ENVIRONMENT=$1
 
 docker compose -f docker-compose.yml build --no-cache
 docker compose -f docker-compose.yml down
